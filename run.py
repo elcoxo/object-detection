7143decf6814347281ac5d0daaf282bdff0c96c7
elcoxo <rakkii7777@gmail.com>
2023-04-25 07:00:26 +0300

    argparse for object class name added

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import cv2
-import torch
 import numpy as np
 import yaml
+import argparse
 import os
 
 def preprocess(img, net):
@@ -11,7 +11,7 @@ def preprocess(img, net):
         
         return outputs
 
-def postprocess(img, outputs):
+def postprocess(img, outputs, object_idx):
         class_ids = []
         confidences = []
         boxes = []
@@ -25,8 +25,8 @@ def postprocess(img, outputs):
             confidence = row[4]
             if confidence > 0.5:
                 scores = row[5:]
-                if scores[15] > 0.5:
-                    class_ids.append(15)
+                if scores[object_idx] > 0.5:
+                    class_ids.append(object_idx)
                     confidences.append(confidence)
                     center_x, center_y, w, h = row[:4]
                     x = int((center_x- w/2)*x_scale)
@@ -51,33 +51,50 @@ def postprocess(img, outputs):
         return img
 
 if __name__== "__main__":
-    net = cv2.dnn.readNetFromONNX('yolov5/models/yolov5s.onnx')
-    cap = cv2.VideoCapture('input/1.mp4')
-    filename = 'input/1.mp4'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", help="Input video e.g. <name>.mp4", default='1.mp4')
+    parser.add_argument("object", help="Coco class name to detect object e.g. cat", default='cat')
+    args = parser.parse_args()
+    filename = args.name
+    objectname = args.object
+    print('File name: ' + filename)
+    print('Object name: ' + objectname)
 
+    net = cv2.dnn.readNetFromONNX('yolov5/models/yolov5s.onnx')
+    cap = cv2.VideoCapture('input/' + filename)
+    
+    
+    
     classes = []
     with open('yolov5/data/coco.yaml', 'r') as file:
         dict = yaml.load(file, Loader=yaml.FullLoader)
         arr = list(dict['names'].items())
         for key in range(len(arr)):
             classes.append(arr[key][1]) # Coco 80 classes name
+        if (classes.index(objectname)):
+            print("Object name found!")
+            object_idx = classes.index(objectname)
+        else:
+            print("Object name not found :(")
 
     if (cap.isOpened() == True):
         width = int(cap.get(3))
         height = int(cap.get(4))
         fps = cap.get(5)
-        print('Weight x h FPS: ', width, 'x', height, fps)
+        print('Weight x Height FPS: ', width, 'x', height, fps)
 
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        video = cv2.VideoWriter(os.path.basename(filename).split(".")[0] + '_output.avi', fourcc, fps, (width,height))
+        video = cv2.VideoWriter('result/' + os.path.basename(filename).split(".")[0] + '_' + 
+                                objectname + '.avi', fourcc, fps, (width,height))
     else:
         print('Can''t load the file, try another one :(')
 
+    print('Processing...')
     while(cap.isOpened()):
         flag, img = cap.read()
         if flag==True:
             cap_blob = preprocess(img, net)
-            output = postprocess(img, cap_blob)
+            output = postprocess(img, cap_blob, object_idx)
             video.write(output)
         else:
             break
@@ -85,4 +102,4 @@ if __name__== "__main__":
     cap.release()
     video.release()
     cv2.destroyAllWindows()
-    print('Done :)')
+    print('Done :)\nFile saved in: result/' + os.path.basename(filename).split(".")[0] + '_' + objectname + '.avi')

d06410e139f781dea8732357cc8ab91ea6a44770
elcoxo <rakkii7777@gmail.com>
2023-04-25 07:34:07 +0300

    small update

--- run.py
+++ run.py
@@ -5,50 +5,50 @@ import argparse
 import os
 
 def preprocess(img, net):
-        blob = cv2.dnn.blobFromImage(img,scalefactor= 1/255,size=(640,640),mean=[0,0,0],swapRB= True, crop= False)
-        net.setInput(blob)
-        outputs = net.forward(net.getUnconnectedOutLayersNames())
-        
-        return outputs
+    blob = cv2.dnn.blobFromImage(img,scalefactor= 1/255,size=(640,640),mean=[0,0,0],swapRB= True, crop= False)
+    net.setInput(blob)
+    outputs = net.forward(net.getUnconnectedOutLayersNames())
+    
+    return outputs
 
 def postprocess(img, outputs, object_idx):
-        class_ids = []
-        confidences = []
-        boxes = []
-        img_width, img_height = img.shape[1], img.shape[0]
-        x_scale = img_width/640
-        y_scale = img_height/640
-        
-        rows = outputs[0].shape[1]
-        for i in range(rows):
-            row = outputs[0][0][i]
-            confidence = row[4]
-            if confidence > 0.5:
-                scores = row[5:]
-                if scores[object_idx] > 0.5:
-                    class_ids.append(object_idx)
-                    confidences.append(confidence)
-                    center_x, center_y, w, h = row[:4]
-                    x = int((center_x- w/2)*x_scale)
-                    y = int((center_y-h/2)*y_scale)
-                    w = int(w * x_scale)
-                    h = int(h * y_scale)
+    class_ids = []
+    confidences = []
+    boxes = []
+    img_width, img_height = img.shape[1], img.shape[0]
+    x_scale = img_width/640
+    y_scale = img_height/640
+    
+    rows = outputs[0].shape[1]
+    for i in range(rows):
+        row = outputs[0][0][i]
+        confidence = row[4]
+        if confidence > 0.5:
+            scores = row[5:]
+            if scores[object_idx] > 0.5:
+                class_ids.append(object_idx)
+                confidences.append(confidence)
+                center_x, center_y, w, h = row[:4]
+                x = int((center_x - w/2) * x_scale)
+                y = int((center_y - h/2) * y_scale)
+                w = int(w * x_scale)
+                h = int(h * y_scale)
 
-                    box = np.array([x, y, w, h])
-                    boxes.append(box)
+                box = np.array([x, y, w, h])
+                boxes.append(box)
 
-        indices = cv2.dnn.NMSBoxes(boxes,confidences,0.5,0.4)
+    indices = cv2.dnn.NMSBoxes(boxes,confidences, 0.5, 0.4)
 
-        for i in indices:
-            x, y, w, h = boxes[i]
-            label = classes[class_ids[i]]
-            #cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,0), 3)
-            cv2.rectangle(img,(0,y),(img_width,0),(0,0,0), -1)
-            cv2.rectangle(img,(0,y),(x,img_height),(0,0,0), -1)
-            cv2.rectangle(img,(0,y+h),(img_width,img_height),(0,0,0), -1)
-            cv2.rectangle(img,(x+w,0),(img_width,img_height),(0,0,0), -1)
+    for i in indices:
+        x, y, w, h = boxes[i]
+        # label = classes[class_ids[i]]
+        #cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,0), 3)
+        cv2.rectangle(img,(0,y),(img_width,0),(0,0,0), -1)
+        cv2.rectangle(img,(0,y),(x,img_height),(0,0,0), -1)
+        cv2.rectangle(img,(0,y+h),(img_width,img_height),(0,0,0), -1)
+        cv2.rectangle(img,(x+w,0),(img_width,img_height),(0,0,0), -1)
 
-        return img
+    return img
 
 if __name__== "__main__":
     parser = argparse.ArgumentParser()
